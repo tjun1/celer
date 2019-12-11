fca1d0a3ef0e0f7ae8137f6b5377ed94088d1751
tjun1 <gaiant.baba@gmail.com>
2019-12-11 20:43:16 +0900

    整形する

--- runtest.sh
+++ runtest.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
+echo "---- cleanup ----"
 . cleanup.sh
 
+echo "---- build & run ----"
 go generate
 go run main.go
 
+echo -e "\n---- results ----"
+
 if [ $? -gt 0 ]; then
-    echo -e "\n------------------------"
+
     echo -e "Failed"
     exit 1
 fi
 
-echo -e "\n------------------------"
 echo -e "Success"
 exit 0
