8bae80657e510c67fb2ad50b1e7cec7a2256f692
tjun1 <gaiant.baba@gmail.com>
2020-04-12 18:01:23 +0900

    作り直し。後片付けまでできる。

--- Makefile
+++ Makefile
@@ -4,40 +4,23 @@ MAIN := main.go
 GOMOD := go.mod
 export GO111MODULE=on
 
+.PHONY: build
 build: $(MAIN)
 	go generate
 	go build -o bin/$(NAME) $<
 
+.PHONY: install
 install: $(MAIN)
 	go generate
 	go build
 	go install
 
+.PHONY: clean
 clean:
-	./cleanup.sh
-	# rm -rf .emacs.d
-	# rm -rf dev{,.sh}
-	# rm -rf setup.sh
-	# rm -rf run
-	# rm -rf docker-compose.yml
-	# rm -rf Dockerfile
-	# rm -rf docker-golang-env
-	# rm -rf docker-clang-9-env
-	# rm -rf update-alternatives-clang-9.sh
-	# rm -rf /tmp/setup.sh
-	# rm -rf /tmp/run
-	# rm -rf /tmp/dev.sh
-	# rm -rf /tmp/clues-theme.el
-	# rm -rf /tmp/emacs-custom.el
-	# rm -rf /tmp/init.el
-	# rm -rf /tmp/.emacs.d
-	# rm -rf /tmp/docker-compose.yml
-	# rm -rf /tmp/Dockrefile
-	# rm -rf /tmp/update-alternatives-clang-9.sh
-	# rm -rf /tmp/docker-golang-env
-	# rm -rf /tmp/docker-clang-9-env
-	# rm celer
+	./scripts/cleanup.sh
 
+
+.PHONY: run
 run: $(MAIN)
 	go run $<
 
--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/rakyll/statik/fs"
-	_ "github.com/tjun1/celer/statik"
+	_ "github.com/tjun1/celer/statik" // こいつが必要
 )
 
 // コードジェネレータが付いている
@@ -20,70 +20,71 @@ func deploy(ltype string) {
 	//log.SetFlags(log.Lshortfile)
 
 	langCollection := make(map[string]string)
-	langCollection["golang"] = "docker-golang-env"
-	langCollection["clang"] = "docker-clang-9-env"
-	langCollection["gcc"] = "docker-gcc-env"
-	langCollection["ruby"] = "docker-ruby-env"
-	langCollection["python"] = "docker-python-env"
+	langCollection["golang"] = "/docker-golang-env"
+	langCollection["clang"] = "/docker-clang-9-env"
+	langCollection["gcc"] = "/docker-gcc-env"
+	langCollection["ruby"] = "/docker-ruby-env"
+	langCollection["python"] = "/docker-python-env"
 
-	var lang string
+	//var lang string
 	var langPath string
 	switch ltype {
 	case "clang":
-		lang = langCollection["clang"]
+		langPath = langCollection["clang"]
 	case "golang":
-		lang = langCollection["golang"]
+		langPath = langCollection["golang"]
 	case "ruby":
-		lang = langCollection["ruby"]
+		langPath = langCollection["ruby"]
 	case "gcc":
-		lang = langCollection["gcc"]
+		langPath = langCollection["gcc"]
 	case "python":
-		lang = langCollection["python"]
+		langPath = langCollection["python"]
 	default:
 		flag.Usage()
 	}
 
-	statikFS, err := fs.New()
+	FS, err := fs.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	f, err := FS.Open(langPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	io.Copy(os.Stdout, f)
+	//fmt.Println(f)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = fs.Walk(statikFS, "/", func(path string, info os.FileInfo, err error) error {
-		if path == "/" {
-			return err
-		}
+	err = fs.Walk(FS, langPath, func(path string, info os.FileInfo, err error) error {
+		// 宛先のディレクトリ
 
-		replacedPath := strings.Replace(path, "/", "", 1)
-		dirpath := filepath.Join("/tmp", replacedPath)
-		if replacedPath == lang {
-			langPath = dirpath
-		}
-		// ディレクトリがなければ作る
+		fmt.Println("読みこんでいるファイル", path)
+		dstPath := filepath.Join(`/tmp`, path)
+		fmt.Println("宛先のファイル", dstPath)
+
+		// path 名が langPath ならディレクトリとして作る
 		if info.IsDir() {
-			_, err := os.Stat(dirpath)
-			if err == nil {
+			err = os.Mkdir(dstPath, 0755)
+			if err != nil {
+				fmt.Print("ディレクトリの作成に失敗")
 				return err
-			} else {
-				err = os.Mkdir(dirpath, 0755)
-				if err != nil {
-					log.Printf("作れませんでした: %s", dirpath)
-					return err
-				} else {
-					//debug
-					// log.Printf("作れた: %s\n", dirpath)
-				}
 			}
-			return err
+			return nil
 		}
-
-		b, err := fs.ReadFile(statikFS, path)
+		// path 名がファイルなら
+		b, err := fs.ReadFile(FS, path)
 		if err != nil {
+			log.Fatal("ファイルが読み込めない")
 			log.Fatal(err)
 			return err
 		}
-
-		err = ioutil.WriteFile(dirpath, b, 0644)
+		err = ioutil.WriteFile(dstPath, b, 0644)
 		if err != nil {
+			log.Fatal("ファイルの作成に失敗しています")
 			log.Fatal(err)
 			return err
 		}
@@ -91,50 +92,69 @@ func deploy(ltype string) {
 		return nil
 	})
 
+	// テンポラリファイルをカレントディレクトリにコピーする
+	tmpPath := filepath.Join(`/tmp`, langPath)
+	fmt.Println("テンポラリのディレクトリ", tmpPath)
+
+	fileList, err := ioutil.ReadDir(tmpPath)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 
-	// TODO: プログラミング言語の種類に対応する
-	//list, err := ioutil.ReadDir("/tmp/docker-golang-env")
-	//fmt.Println(langPath)
-	list, err := ioutil.ReadDir(langPath)
+	cPath, err := os.Getwd()
 	if err != nil {
+		log.Fatal("カレントディレクトリが習得できない")
 		log.Fatal(err)
 		os.Exit(1)
 	}
+	for _, finfo := range fileList {
 
-	for _, finfo := range list {
-		srcPath := filepath.Join(langPath, finfo.Name())
-		err = os.Rename(srcPath, finfo.Name())
+		fmt.Println("finfo.Name(): ", finfo.Name())
+		tmpFile := filepath.Join(tmpPath, finfo.Name())
+		fmt.Println(tmpFile)
+		targetFile := filepath.Join(cPath, finfo.Name())
+		fmt.Println("cPath: ", cPath)
+		fmt.Println("target: ", targetFile)
+		err = os.Rename(tmpFile, targetFile)
 		if err != nil {
+			log.Fatal("リネームコピーできなかった")
 			log.Fatal(err)
 			os.Exit(1)
 		}
 	}
 
+	// 片付け
+	if err := os.RemoveAll(tmpPath); err != nil {
+		log.Fatal("片付けできなかった")
+		log.Fatal(err)
+		os.Exit(1)
+	}
 	log.Println("...done")
 	os.Exit(0)
+
 }
 
 func Show(path string, info os.FileInfo, err error) error {
 	fmt.Println(path, info.Name(), err)
 	return nil
+
 }
 
-func main() {
-	const CELER_VERSION = "v0.9.9"
+func
+main() {
+	const CELER_VERSION = "v0.9.12"
 
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage: %s <option> <value>\n\n", os.Args[0])
 		flag.PrintDefaults()
 	}
+
 	var (
-		ltype   = flag.String("t", "golang", "Language type Choice")
+		ltype    = flag.String("t", "golang", "Language type Choice")
 		typeList = flag.Bool("l", false, "List language type")
-		debug   = flag.Bool("debug", false, "Debug Mode enabled?")
-		version = flag.Bool("v", false, "Show version")
+		debug    = flag.Bool("debug", false, "Debug Mode enabled?")
+		version  = flag.Bool("v", false, "Show version")
 	)
 	flag.Parse()
 
@@ -155,18 +175,22 @@ func main() {
 
 	switch *ltype {
 	case "golang":
+		fmt.Println("golang deploying...")
 		deploy("golang")
 	case "clang":
+		fmt.Println("clang deploying")
 		deploy("clang")
 	case "ruby":
+		fmt.Println("ruby deploying")
 		deploy("ruby")
 	case "gcc":
+		fmt.Println("gcc deploying")
 		deploy("gcc")
 	case "python":
+		fmt.Println("python deploying")
 		deploy("python")
 	default:
 		flag.Usage()
 	}
-
 
 }
